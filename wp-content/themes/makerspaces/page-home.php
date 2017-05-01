982d55a1250d99e8dac7931950b414c1bcbaa3bf
David Beauchamp <dbeauchamp@makermedia.com>
2017-05-01 18:15:30 +0200

    Finished home page

--- wp-content/themes/makerspaces/page-home.php
+++ wp-content/themes/makerspaces/page-home.php
@@ -7,27 +7,50 @@
 
 get_header(); ?>
 
+<div class="home-page-wrap">
+
   <div class="container">
 
     <div class="row">
 
       <div class="col-xs-12">
 
-        
-
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php the_content(); ?>
-          <?php
-            // If comments are open or we have at least one comment, load up the comment template
-            if ( comments_open() || '0' != get_comments_number() )
-              comments_template();
-          ?>
-        <?php endwhile; // end of the loop. ?>
+        <h1>Makerspaces represent the democratization of design, engineering, fabrication, and education.</h1>        
 
       </div>
 
     </div>
 
   </div>
+
+  <div class="home-page-hero" style="background: url(/wp-content/themes/makerspaces/img/home-header.png) no-repeat center center;"></div>
+
+  <div class="container home-page-body">
+
+    <div class="row">
+
+      <div class="col-xs-12 col-sm-6">
+
+        <p>To describe them simply, makerspaces are community centers with tools. Makerspaces combine manufacturing equipment, community, and education for the purposes of enabling community members to design, prototype and create manufactured works that wouldn’t be possible to create with the resources available to individuals working alone.</p>
+
+      </div>
+
+      <div class="col-xs-12 col-sm-6">
+
+        <p>These spaces can take the form of loosely-organized individuals sharing space and tools, for-profit companies, non-profit corporations, organizations affiliated with or hosted within schools, universities or libraries, and more. All are united in the purpose of providing access to equipment, community, and education, and all are unique in exactly how they are arranged to fit the purposes of the community they serve.</p>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="home-page-bottom text-center">
+
+    <h2>Makerspaces represent the democratization of design, engineering, fabrication, and education.</h2>
+
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
