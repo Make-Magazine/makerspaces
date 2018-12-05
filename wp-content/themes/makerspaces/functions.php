7aaec1ca96edbfa05ffde1ce89aed1324a466a09
Rio Roth-Barrerio <rio@makermedia.com>
2018-12-05 11:10:40 -0800

    remove wp login logout

--- wp-content/themes/makerspaces/functions.php
+++ wp-content/themes/makerspaces/functions.php
@@ -157,56 +157,13 @@ function _makerspaces_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	// only load on the image attachemnt pages
+	// only load on the image attachment pages
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( '_makerspaces-keyboard-image-navigation', THEME_DIR_URI . '/includes/js/min/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 
 }
 add_action( 'wp_enqueue_scripts', '_makerspaces_scripts' );
-
-
-/** Set up the Ajax Logout */
-add_action( 'wp_ajax_mm_wplogout',        'MM_wordpress_logout' );
-add_action( 'wp_ajax_nopriv_mm_wplogout', 'MM_wordpress_logout' );
-
-function MM_wordpress_logout(){
-    //check_ajax_referer( 'ajax-logout-nonce', 'ajaxsecurity' );
-    wp_logout();
-    ob_clean(); // probably overkill for this, but good habit
-    wp_send_json_success();
-}
-
-add_action( 'wp_ajax_mm_wplogin', 'MM_WPlogin' );
-add_action( 'wp_ajax_nopriv_mm_wplogin', 'MM_WPlogin' );
-
-/** Set up the Ajax WP Login */
-function MM_WPlogin(){
-  //check_ajax_referer( 'ajax-login-nonce', 'ajaxsecurity' );
-  global $wpdb; // access to the database
-
-  //use auth0 plugin to log people into wp
-  $a0_plugin  = new WP_Auth0();
-  $a0_options = WP_Auth0_Options::Instance();
-  $users_repo = new WP_Auth0_UsersRepo( $a0_options );
-  $users_repo->init();
-
-  $login_manager = new WP_Auth0_LoginManager( $users_repo, $a0_options );
-  $login_manager->init();
-
-  //get the user information passed from auth0
-  $userinput     = filter_input_array(INPUT_POST);
-  $userinfo      = (object) $userinput['auth0_userProfile'];
-  $userinfo->email_verified = true;
-  $access_token = filter_input(INPUT_POST, 'auth0_access_token', FILTER_SANITIZE_STRING);
-  $id_token     = filter_input(INPUT_POST, 'auth0_id_token', FILTER_SANITIZE_STRING);
-
-  if($login_manager->login_user( $userinfo, $id_token, $access_token)) {
-    wp_send_json_success();
-  }else{
-    wp_send_json_error();
-  }
-}
 
 
 /**
