9d36390ad610f54085b2bc3fc4cff34ced7d8197
Rio Roth-Barrerio <rio@makermedia.com>
2018-12-18 13:49:49 -0800

    update recaptcha for playbook form

--- wp-content/themes/makerspaces/includes/js/min/scripts.js
+++ wp-content/themes/makerspaces/includes/js/min/scripts.js
@@ -283,69 +283,19 @@ jQuery(document).ready( function( $ ) {
 });
 
 
-var recaptchaKey = '6Lf_-kEUAAAAAHtDfGBAleSvWSynALMcgI1hc_tP';
-onloadCallback = function() {
-  if ( jQuery('#recapcha-footer-desktop').length ) {
-    grecaptcha.render('recapcha-footer-desktop', {
-      'sitekey' : recaptchaKey,
-      'callback' : onSubmitFooterDesk
-    });
-  }
-  if ( jQuery('#recapcha-footer-mobile').length ) {
-    grecaptcha.render('recapcha-footer-mobile', {
-      'sitekey' : recaptchaKey,
-      'callback' : onSubmitFooterMob
-    });
-  }
-  if ( jQuery('#recapcha-header').length ) {
-    grecaptcha.render('recapcha-header', {
-      'sitekey' : recaptchaKey,
-      'callback' : onSubmitHeader
-    });
-  }
-  if ( jQuery('#recapcha-playbook').length ) {
-    grecaptcha.render('recapcha-playbook', {
-      'sitekey' : recaptchaKey,
-      'callback' : onSubmitPlaybook
-    });
-  }
-};
-//header desktop
-var onSubmitHeader = function(token) {
-  jQuery.post('https://secure.whatcounts.com/bin/listctrl', jQuery('.whatcounts-signup').serialize());
-  jQuery('.nl-thx').trigger('click');
+function playbookSignup() {
+
+	jQuery.post('https://secure.whatcounts.com/bin/listctrl', jQuery('.playbook-sub-form').serialize());
+
+   jQuery('.nl-thx').trigger('click');
+
 }
-jQuery(document).on('submit', '.whatcounts-signup', function (e) {
-  e.preventDefault();
-  onSubmitHeader();
-});
-//footer desktop
-var onSubmitFooterDesk = function(token) {
-  jQuery.post('https://secure.whatcounts.com/bin/listctrl', jQuery('.whatcounts-signup1f').serialize());
-  jQuery('.nl-thx').trigger('click');
-}
-jQuery(document).on('submit', '.whatcounts-signup1f', function (e) {
-  e.preventDefault();
-  onSubmitFooterDesk();
-});
-//footer mobile
-var onSubmitFooterMob = function(token) {
-  jQuery.post('https://secure.whatcounts.com/bin/listctrl', jQuery('.whatcounts-signup1m').serialize());
-  jQuery('.nl-thx').trigger('click');
-}
-jQuery(document).on('submit', '.whatcounts-signup1m', function (e) {
-  e.preventDefault();
-  onSubmitFooterMob();
-});
-//playbook page
-var onSubmitPlaybook = function(token) {
-  jQuery.post('https://secure.whatcounts.com/bin/listctrl', jQuery('.playbook-sub-form').serialize());
-  jQuery('.nl-thx').trigger('click');
-}
+
 jQuery(document).on('submit', '.playbook-sub-form', function (e) {
   e.preventDefault();
-  onSubmitPlaybook();
-});;//!!
+  grecaptcha.execute();
+});
+;//!!
 //!! includes/js/skip-link-focus-fix.js
 ( function() {
   var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
--- wp-content/themes/makerspaces/page-playbook.php
+++ wp-content/themes/makerspaces/page-playbook.php
@@ -44,22 +44,26 @@ get_header(); ?>
             $isSecure = "https://";
           }
         ?>
-        <form class="playbook-sub-form" action="https://secure.whatcounts.com/bin/listctrl" method="POST">
-          <input type="hidden" name="slid" value="6B5869DC547D3D467B33E192ADD9BE4B" /><!-- MakerPro -->
+        <form class="playbook-sub-form" action="https://secure.whatcounts.com/bin/listctrl" method="POST" class="validate" target="_blank">
+          <input type="hidden" name="slid" id="list_6B5869DC547D3D46B52F3516A785F101_yes" class="slid" value="6B5869DC547D3D467B33E192ADD9BE4B" /><!-- MakerPro -->
           <input type="hidden" name="cmd" value="subscribe" />
           <input type="hidden" name="custom_source" value="makerspace-playbook" />
           <input type="hidden" name="custom_incentive" value="playbookPDF" />
           <input type="hidden" name="custom_url" value="<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" />
           <input type="hidden" id="format_mime" name="format" value="mime" />
           <input type="hidden" name="custom_host" value="<?php echo $_SERVER["HTTP_HOST"]; ?>" />
-          <div id="recapcha-playbook" class="g-recaptcha" data-size="invisible"></div>
           <input name="name" placeholder="YOUR NAME" required type="name"><br>
           <input name="email" placeholder="YOUR EMAIL" required type="email"><br>
           <input value="REQUEST A FREE COPY" class="btn btn-lt-blue btn-block" type="submit">
         </form>
+         <!-- reCaptcha element -->
+         <div id="recaptcha" class="g-recaptcha"
+            data-sitekey="6Lf_-kEUAAAAAHtDfGBAleSvWSynALMcgI1hc_tP"
+            data-callback="playbookSignup"
+            data-size="invisible">
+         </div>
 
-      </div>
-
+           
       <div class="col-xs-12 col-sm-6 playbook-page-body-r">
 
         <p>CONTENTS</p>
@@ -69,7 +73,7 @@ get_header(); ?>
         <p><strong>Places</strong><br />
           making a space more conducive to a community that makes together</p>
 
-        <p><strong>Tools & Materials</strong><br />
+        <p><strong>Tools &amp; Materials</strong><br />
           inventory, budgets, and strategies (see also High School Makerspace Tools & Materials: a companion document detailing the uses and costs of a fully stocked inventory for an in-school Makerspace.)</p>
 
         <p><strong>Safety</strong><br />
