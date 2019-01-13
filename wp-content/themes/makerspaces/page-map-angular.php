86729faed56ba7cbd8257b9c074b9cd32f62619e
Travis Stone <travis@makermedia.com>
2019-01-13 11:44:34 -0800

    - test new map JS

--- wp-content/themes/makerspaces/page-map-angular.php
+++ wp-content/themes/makerspaces/page-map-angular.php
@@ -6,150 +6,146 @@
  */
 
 get_header(); ?>
-<div class="makerspaces-map-wrp" ng-app="makerSpacesApp" ng-strict-di>
-  <div class="container">
-    <div class="row map-header">
-      <div class="col-xs-12 col-lg-12">
-        <h1>Makerspaces Directory</h1>
-			<p><?php echo the_content(); ?></p>
+<div class="container">
+   <div class="row">
+      <div class="col-md-12">
+         <h1>Makerspaces Directory</h1>
       </div>
-    </div>
-
-    <div class="row map-app-container">
-      <?php echo do_shortcode( '[makemap form="2" searchtext="Find a Makerspace"]' );?>
-    </div>
-  </div>
-	
-  <div class="container-fluid light-blue">
-	  <div class="container">
-			<div class="row">
-				<div class="col-md-3 col-sm-6 col-xs-12 makerspace-bottom-nav">
-					<h4>Join our global network of makerspaces</h4>
-					<a href="/register"><button class="btn blue-btn">Add your makerspace</button></a>
-				</div>
-				<div class="col-md-3 col-sm-6 col-xs-12 makerspace-bottom-nav">
-					<h4>See an error or need to update your info?</h4>
-               <a href="/edit-your-makerspace"><button class="btn blue-btn">Manage your listing</button></a>					
-				</div>
-				<div class="col-md-3 col-sm-6 col-xs-12 makerspace-bottom-nav">
-					<h4>Get a free PDF guide on starting a makerspace</h4>
-					<a href="/playbook"><button class="btn blue-btn">Download the playbook</button></a>
-				</div>
-				<div class="col-md-3 col-sm-6 col-xs-12 makerspace-bottom-nav">
-					<h4>Start your own makerspace today</h4>
-					<a href="https://learn.make.co/courses/starting-a-school-makerspace/info"><button class="btn blue-btn">Take the course</button></a>
-				</div>
-			</div>
-		</div>
-  </div>  
-	
-	<div class="container makerspace-news">
-		<div class="row posts-feeds-wrapper">
-		  <h2>Makerspace News from <img class="logo" src="https://make.co/wp-content/themes/memberships/img/make_logo.svg" /> magazine</h2>
-
-          <?php
-          $rss = fetch_feed('https://makezine.com/tag/makerspaces/feed/');
-          if (!is_wp_error($rss)) :
-            $maxitems = $rss -> get_item_quantity(4); //gets latest 5 items, this can be changed to suit your requirements
-            $rss_items = $rss -> get_items(0, $maxitems);
-          endif;
-
-          //grabs our post thumbnail image
-          function get_first_image_url($html) {
-            if (preg_match('/<img.+?src="(.+?)"/', $html, $matches)) {
-              return $matches[1];
-            }
-          }
-			 
-			 // get just the text
-			 function get_summary($html) {
-				 $summary = preg_replace('/<a[^>]*>([\s\S]*?)<\/a[^>]*>/', '', $html);
-				 $summary = strip_tags(str_replace('The post appeared first on .', '', $summary));
-				 return $summary;
-			 }
-
-          //shortens description
-          function shorten($string, $length) {
-            $suffix = '&hellip;';
-            $short_desc = trim(str_replace(array("\r", "\n", "\t"), ' ', strip_tags($string)));
-            $desc = trim(substr($short_desc, 0, $length));
-            $lastchar = substr($desc, -1, 1);
-            if ($lastchar == '.' || $lastchar == '!' || $lastchar == '?')
-            $suffix = '';
-            $desc .= $suffix;
-            return $desc;
-          }
-          ?>
-            <?php
-            if ($maxitems == 0) echo '<li>No items.</li>';
-            else foreach ( $rss_items as $item ) :
-            ?>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-				 <div class="post-feed">
-              <a class="full-link" href="<?php echo esc_url($item -> get_permalink()); ?>" target="_blank">
-					  <div class="title">
-						 <img src="<?php echo get_first_image_url($item -> get_content()); ?>" alt="Makerspace post featured image">
-						 <p class="p-title"><?php echo esc_html($item -> get_title()); ?></p>
-						 <p><?php echo shorten(get_summary($item -> get_content()), 100); ?></p>
-					  </div>
-					</a>
-				 </div>
-            </div>
-            <?php endforeach; ?>
-            <a class="all-projects-title" href="http://makezine.com/tag/makerspaces/" target="_blank"><button class="btn blue-btn">See more articles</button></a>
-	
-  
-	  </div>
-	</div>
+   </div>
+   <div class="row">
+      <div class="col-md-12">
+         <form action="">
+            <label for="">Find a Makerspace
+               <input type="search">
+            </label>
+            <button>Search</button>
+         </form>
+         <div id="map" style="height: 768px;"></div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-12">
+         Table
+      </div>
+   </div>
 </div>
-	
-	      <?php /*
-        <div class="posts-feeds-wrapper col-xs-12 col-sm-6">
-          <?php
-          $rss = fetch_feed('https://makezine.com/projects/feed/');
-          if (!is_wp_error($rss)) :
-            $maxitems = $rss -> get_item_quantity(5); //gets latest 5 items, this can be changed to suit your requirements
-            $rss_items = $rss -> get_items(0, $maxitems);
-          endif;
 
-          //grabs our post thumbnail image
-          function get_first_image_url_p($html) {
-            if (preg_match('/<img.+?src="(.+?)"/', $html, $matches)) {
-              return $matches[1];
-            }
-          }
 
-          //shortens description
-          function shorten_p($string, $length) {
-            $suffix = '&hellip;';
-            $short_desc = trim(str_replace(array("\r", "\n", "\t"), ' ', strip_tags($string)));
-            $desc = trim(substr($short_desc, 0, $length));
-            $lastchar = substr($desc, -1, 1);
-            if ($lastchar == '.' || $lastchar == '!' || $lastchar == '?')
-            $suffix = '';
-            $desc .= $suffix;
-            return $desc;
-          }
-          ?>
-          <h3 class="feed-title">
-            <i class="fa fa-newspaper-o feed-icon"></i> Latest Projects
-          </h3>
-          <ul class="posts-feeds">
-            <?php
-            if ($maxitems == 0) echo '<li>No items.</li>';
-            else foreach ( $rss_items as $item ) :
-            ?>
-            <li class="post-feed">
-              <a class="full-link" href="<?php echo esc_url($item -> get_permalink()); ?>" target="_blank"></a>
 
-              <div class="title">
-                <img src="<?php echo get_first_image_url_p($item -> get_content()); ?>" alt="Makerspace post featured image">
-                <p class="p-title"><?php echo esc_html($item -> get_title()); ?></p>
-              </div>
-            </li>
-            <?php endforeach; ?>
-            <a class="all-projects-title" href="http://makezine.com/tag/makerspaces/" target="_blank">See All Projects</a>
-          </ul>
-        </div> */ ?>
+
+
+<script>
+
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '400px';
+  } else {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '800px';
+  }
+}
+
+detectBrowser();
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 29.1070772, lng: -24.2299966},
+          zoom: 2
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Location found.');
+            // infoWindow.open(map);
+            map.setCenter(pos);
+            map.setZoom(8);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+          
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      //   infoWindow.setPosition(pos);
+      //   infoWindow.setContent(browserHasGeolocation ?
+      //                         'Error: The Geolocation service failed.' :
+      //                         'Error: Your browser doesn\'t support geolocation.');
+      //   infoWindow.open(map);
+         console.log(browserHasGeolocation, infoWindow, pos);
+      }
+
+      var detailWindow = '<h4>test</h4>';
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+      jQuery.get( "/wp-json/makemap/v1/mapdata/2", function( data ) {
+         //console.log(data);
+         //$( ".result" ).html( data );
+         //alert( "Load was performed." );
+         // jQuery.each(data.Locations, function(idx, el){
+         //    console.log(idx,el);
+         // var latLng = new google.maps.LatLng(el.mmap_lat,el.mmap_lng);
+         // var marker = new google.maps.Marker({
+         //    position: latLng,
+         //    label: labels[idx % labels.length],
+         //    map: map
+         // });
+
+         var markers = data.Locations.map(function(location, i) {
+            console.log(location);
+            var latLng = {lat: parseFloat(location.mmap_lat), lng: parseFloat(location.mmap_lng)};
+          var marker =  new google.maps.Marker({
+            position: latLng,
+            label: ''//labels[i % labels.length]
+          });
+          marker.addListener('click', function() {
+            //console.log(location.mmap_eventname);
+            var myWindow = new google.maps.InfoWindow({
+               content: '<div style=""><h4>'+location.mmap_eventname+'</h4><p><a href="'+location.mmap_url+'">'+location.mmap_url+'</a></p><p>'+location.mmap_type+'</p></div>'
+            });
+
+            myWindow.open(map, marker);
+         });
+          return marker;
+        });
+
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+         // });
+      });
+
+
+
+
+      </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+    </script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDtWsCdftU2vI9bkZcwLxGQwlYmNRnT2VM&callback=initMap"></script>
+
 <?php get_footer(); ?>
 
